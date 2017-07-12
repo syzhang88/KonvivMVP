527b49890b3f678922ade1bb4c0812feef77e1b4
Samuel Zhang <szhang88@berkeley.edu>
2017-07-12 14:53:55 -0700

    got bucket filling working

--- app/buckets.js
+++ app/buckets.js
@@ -34,6 +34,20 @@ var nameBuckets = {
 
 
 // Classifies which bucket a certain transaction belongs to
+exports.selectBuckets = function selectBuckets (transactions) {
+    // console.log('New Selection:');
+    buckets = {};
+
+    for (transaction in transactions) {
+        var bucket = selectBucket(transaction);
+        var newPostKey = transaction.transaction_id;
+        var postData = {}
+        postData[newPostKey] = transaction;
+        buckets[bucket] = postData
+    };
+    return buckets;
+}
+
 exports.selectBucket = function selectBucket (transaction) {
     // console.log('New Selection:');
     var bucket = 'General Spending';
@@ -58,6 +72,7 @@ exports.selectBucket = function selectBucket (transaction) {
 
 // Estimates the size of a bucket given transactions from a given interval of
 // days, which is passed in as estimationPeriod
+
 exports.estimateSize = function estimateSize (transactions, estimationPeriod) {
     console.log("calculating buckets sizes now...");
 
@@ -98,77 +113,77 @@ exports.estimateSize = function estimateSize (transactions, estimationPeriod) {
 
 /*** REMAINING FUNCTIONS HAVE NOT BEEN TESTED AND MAY BE BUGGY ***/
 
-// // If the bucket the user is moving this transaction to matches the last
-// // bucket the user moved a transaction with this name to (i.e., the user
-// // moves transactions with the same name to the same bucket twice in a row),
-// // automatically store that bucket in reclassifiedTransactions to categorize all
-// // transactions sharing this name to that bucket in the future.
-// function reclassification (transaction, oldBucket, newBucket) {
-//     if (oldBucket != newBuckets) {
-//         if (trackReclassifications[transaction.name] == newBucket) {
-//             reclassifiedTransactions[transaction.name] = newBucket;
-//         } else {
-//             trackReclassifications[transaction.name] = newBucket;
-//         }
-//     }
-// };
-//
-// // oldDbPath and newDbPath each are firebase.database().ref('...')
-// // oldDbPath and newDbPath are manually entered because of abstraction, to
-// // simplify modifying firebase structure
-// exports.moveTransaction = function moveTransaction (transaction, oldBucketPath, newBucketPath, path) {
-//     // Check if bucket is real
-//     var names = Object.keys(nameBuckets);
-//     if (names.indexOf(oldBucket) < 0 || names.indexOf(newBucket) < 0 ) throw "not a bucket";
-//
-//     reclassification(transaction, oldBucket, newBucket);
-//     var newPostKey = transaction.transaction_id;
-//     var postData = {}
-//
-//     postData[newPostKey] = transaction;
-//
-//     //add transaction to new bucket
-//     firebase.database().ref(path + newBucket).update(postData);
-//
-//     //delete transaction from old bucket
-//     firebase.database().ref(path + oldBucket).remove(postData);
-// }
-//
-// // moveMoney returns false IF you subtract more money from a bucket than you
-// // have remaining in it
-// exports.moveMoney = function moveMoney (amount, oldBucketPath, newBucketPath) {
-//     var newPostKey = transaction.transaction_id;
-//     var oldBucket = {}
-//     var newBucket = {}
-//
-//     //subtract from this bucket
-//     firebase.database().ref(oldBucketPath).once('value').then(function(snapshot) {
-//         if (snapshot.val()['Remaining'] - amount > 0) {
-//             oldBucket['Remaining'] = snapshot.val()['Remaining'] - amount;
-//             oldBucket['Total'] = snapshot.val()['Total'] - amount;
-//         } else {
-//             return false;
-//         }
-//     });
-//     firebase.database().ref(oldBucketPath).update(oldBucket);
-//
-//     //add to that bucket
-//     firebase.database().ref(newBucketPath).once('value').then(function(snapshot) {
-//         newBucket['Remaining'] = snapshot.val()['Remaining'] + amount;
-//         newBucket['Total'] = snapshot.val()['Total'] + amount;
-//     });
-//     firebase.database().ref(newBucketPath).update(newBucket);
-//
-//     return true;
-// }
-//
-// // Each bucket has a key named 'Name' in its hashtable (dictionary) underneath
-// // the branch /bucketMoney on Firebase. You can change this name here
-// exports.renameBucket = function renameBucket (newName, oldName) {
-//     for (var key in nameBuckets) {
-//         if (nameBuckets[key] == oldName) {
-//             nameBuckets[key] = newName;
-//             console.log('bucket ' + oldName + ' has been named ' + newName);
-//         }
-//     }
-// }
+// If the bucket the user is moving this transaction to matches the last
+// bucket the user moved a transaction with this name to (i.e., the user
+// moves transactions with the same name to the same bucket twice in a row),
+// automatically store that bucket in reclassifiedTransactions to categorize all
+// transactions sharing this name to that bucket in the future.
+function reclassification (transaction, oldBucket, newBucket) {
+    if (oldBucket != newBuckets) {
+        if (trackReclassifications[transaction.name] == newBucket) {
+            reclassifiedTransactions[transaction.name] = newBucket;
+        } else {
+            trackReclassifications[transaction.name] = newBucket;
+        }
+    }
+};
+
+// oldDbPath and newDbPath each are firebase.database().ref('...')
+// oldDbPath and newDbPath are manually entered because of abstraction, to
+// simplify modifying firebase structure
+exports.moveTransaction = function moveTransaction (transaction, oldBucketPath, newBucketPath, path) {
+    // Check if bucket is real
+    var names = Object.keys(nameBuckets);
+    if (names.indexOf(oldBucket) < 0 || names.indexOf(newBucket) < 0 ) throw "not a bucket";
+
+    reclassification(transaction, oldBucket, newBucket);
+    var newPostKey = transaction.transaction_id;
+    var postData = {}
+
+    postData[newPostKey] = transaction;
+
+    //add transaction to new bucket
+    firebase.database().ref(path + newBucket).update(postData);
+
+    //delete transaction from old bucket
+    firebase.database().ref(path + oldBucket).remove(postData);
+}
+
+// moveMoney returns false IF you subtract more money from a bucket than you
+// have remaining in it
+exports.moveMoney = function moveMoney (amount, oldBucketPath, newBucketPath) {
+    var newPostKey = transaction.transaction_id;
+    var oldBucket = {}
+    var newBucket = {}
+
+    //subtract from this bucket
+    firebase.database().ref(oldBucketPath).once('value').then(function(snapshot) {
+        if (snapshot.val()['Remaining'] - amount > 0) {
+            oldBucket['Remaining'] = snapshot.val()['Remaining'] - amount;
+            oldBucket['Total'] = snapshot.val()['Total'] - amount;
+        } else {
+            return false;
+        }
+    });
+    firebase.database().ref(oldBucketPath).update(oldBucket);
+
+    //add to that bucket
+    firebase.database().ref(newBucketPath).once('value').then(function(snapshot) {
+        newBucket['Remaining'] = snapshot.val()['Remaining'] + amount;
+        newBucket['Total'] = snapshot.val()['Total'] + amount;
+    });
+    firebase.database().ref(newBucketPath).update(newBucket);
+
+    return true;
+}
+
+// Each bucket has a key named 'Name' in its hashtable (dictionary) underneath
+// the branch /bucketMoney on Firebase. You can change this name here
+exports.renameBucket = function renameBucket (newName, oldName) {
+    for (var key in nameBuckets) {
+        if (nameBuckets[key] == oldName) {
+            nameBuckets[key] = newName;
+            console.log('bucket ' + oldName + ' has been named ' + newName);
+        }
+    }
+}
