a75678c81808f7bff0ab38ad40e7aba78188dc91
Shantanu Datta <dattausc@gmail.com>
2017-07-31 15:46:50 -0700

    backend for new database structure with bucket classes working

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/*** Any documentation here not labelled "Sam" is from Plaid's original
-    Quickstart code. ***/
-
+// Documentation by Sam Zhang. Any mistakes or inaccuracies are his own.
 'use strict';
 
 var envvar = require('envvar');
@@ -11,16 +9,17 @@ var plaid = require('plaid');
 var buckets = require('./buckets');
 var admin = require("firebase-admin");
 var firebase = require("firebase");
+var serviceAccount = require("./konvivandroid-firebase-adminsdk-re0l3-f09e6af5d7.json");
 
 const SIX_MONTHS = 6;
 const ONE_MONTH = 1;
 
 /*** SETTING UP FIREBASE, PLAID, AND EXPRESS ***/
 
-// Sam: Begin Firebase code for configuration, initialization, and authentication.
-// Currently, we are linking to DEEP's Firebase database (KonvivAndroid), no
-// longer my own.
-// Here is its link: https://console.firebase.google.com/project/konvivandroid/database/data
+// Begin Firebase code for configuration, initialization, and authentication.
+
+// Currently, we are linking to DEEP's Firebase database (KonvivAndroid):
+// https://console.firebase.google.com/project/konvivandroid/database/data
 var config = {
   apiKey: "AIzaSyASB9RhrUzNme-rGkVrzEXmF3nL7PwMgvQ",
   authDomain: "konvivandroid.firebaseapp.com",
@@ -30,33 +29,33 @@ var config = {
   messagingSenderId: "41760220514",
 };
 
-var serviceAccount = require("./konvivandroid-firebase-adminsdk-re0l3-f09e6af5d7.json");
-
+// Initialize ADMIN Firebase client
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://konvivandroid.firebaseio.com"
 });
 
+// Initialize normal Firebase client
 firebase.initializeApp(config);
-// Sam: End Firebase setup
+// End Firebase setup
 
-// Sam: Begin Plaid code for configuration, initialization, and authentication
+// Begin Plaid code for configuration, initialization, and authentication
 var APP_PORT = envvar.number('APP_PORT', Number(process.env.PORT || 8000 ));
 var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID', '593981e0bdc6a401d71d87b5');
 var PLAID_SECRET = envvar.string('PLAID_SECRET', '271426f90259600c6bf365d6b0f0aa');
 var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY', '9f4ef21fdb37b5c0e3f80290db7716');
 var PLAID_ENV = envvar.string('PLAID_ENV', 'development');
 
-// Initialize the Plaid client
+// Initialize Plaid client
 var client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments[PLAID_ENV]
 );
-// Sam: End Plaid setup
+// End Plaid setup
 
-// Sam: Express setup
+// Express setup
 var app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -65,16 +64,12 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.set('public token', null);
-// app.set('item id', null);
-// app.set('firebase', firebase.initializeApp(config));
-// Sam: End Express setup
+// End Express setup
 
-// Sam: API routes setup
-// get an instance of the router for api routes
+// AI routes set up: get an instance of the router for api routes
 var apiRoutes = express.Router();
 
-/*** Sam: Here is where we define actual RESTful calls (Using Express I believe;
-    check on this) ***/
+// Here is where we define actual RESTful calls, using Express:
 
 app.get('/', function(request, response, next) {
     console.log("app loading...");
@@ -101,21 +96,17 @@ app.get('/newuser.ejs', function(request, response, next) {
 });
 
 app.post('/log_in', function(request, response, next) {
-    console.log('Attempting log in...');
+    console.log('attempting login...');
     var success = {
         login: false
     }
-
-    // gets object to database service
-    // var database = firebase.database();
     var username = request.body.username;
     var password = request.body.password;
 
     firebase.auth().signInWithEmailAndPassword(username, password).then(function() {
         var user = firebase.auth().currentUser;
-        // grabs admin session token
+        // Grab individual user session token
         user.getIdToken().then(function(token) {
-            console.log('successfully logged into firebase');
             success = {
                 login: true,
                 token: token,
@@ -123,9 +114,9 @@ app.post('/log_in', function(request, response, next) {
             };
             firebase.auth().signOut();
             response.json(success);
-            console.log("LOG IN SUCCEEDED");
+            console.log('successfully logged into Firebase');
         }).catch(function(error) {
-            // Handle Errors here.
+            // Handle Errors here
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log('failed to create user: ' + errorMessage);
@@ -139,12 +130,12 @@ app.post('/log_in', function(request, response, next) {
             login: false
         };
         response.json(success);
-        console.log("LOG IN FAILED");
+        console.log("login attempt to Firebase has failed");
     });
 });
 
 app.post('/sign_up', function(request, response, next) {
-    console.log('Attempting sign up...');
+    console.log('attempting sign up...');
 
     // gets object to database service
     var database = firebase.database();
@@ -161,13 +152,13 @@ app.post('/sign_up', function(request, response, next) {
                 error: null
             });
             firebase.auth().signOut();
-            console.log('successfully created user in firebase: ' + user.uid);
+            console.log('successfully created user in Firebase: ' + user.uid);
         });
     }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log('failed to create user: ' + errorMessage);
+        console.log('failed to create user in Firebase: ' + errorMessage);
         response.json({
             login: false,
             error: errorMessage
@@ -199,7 +190,7 @@ apiRoutes.use(function(request, response, next) {
                 next();
             });
         }).catch(function(error) {
-            console.log('Failed to authenticate token.');
+            console.log('failed to authenticate token.');
             return response.json({
                 error: error,
                 message: 'Failed to authenticate token.'
@@ -214,7 +205,9 @@ apiRoutes.use(function(request, response, next) {
 });
 
 apiRoutes.post('/get_access_token', function(request, response, next) {
-    console.log('Getting access token...');
+    // We HAVE to store the access token, so that Plaid does not think the
+    // user is a new user logging in each time and create duplicate transactions
+    console.log('getting access token...');
 
     app.set('public token', request.body.publicToken);
     client.exchangePublicToken(app.get('public token'), function(error, tokenResponse) {
@@ -225,17 +218,13 @@ apiRoutes.post('/get_access_token', function(request, response, next) {
             error: msg
           });
         }
-        // Sam: We HAVE to store the access token, so that Plaid does not think the
-        // user is a new user logging in each time.
 
-        console.log('LOADING Access Token: ' + tokenResponse.access_token);
-
+        console.log('loading Access Token: ' + tokenResponse.access_token);
         admin.database().ref('users/' + request.body.userId).set({
             user_token: tokenResponse.access_token,
             item_id: tokenResponse.item_id
         });
 
-        // console.log('Item ID: ' + ITEM_ID);
         response.json({
           'error': false
         });
@@ -261,21 +250,18 @@ apiRoutes.post('/accounts', function(request, response, next) {
      // [object Object],[object Object],[object Object],[object Object]
      // These are the different checking/cc banking accounts
 
-    // Sam: Begin admin section for updating account info
+    // Admin section for updating account info on Firebase
     var postData = {
         'accounts': authResponse.accounts
     };
-
     admin.database().ref('users/' + request.body.userId).update(postData);
     console.log('posted item for: ' + request.body.userId);
-    // Sam: End admin section
 
     response.json({
       error: false,
       accounts: authResponse.accounts,
       numbers: authResponse.numbers,
     });
-
   });
 });
 
@@ -310,8 +296,6 @@ apiRoutes.post('/item', function(request, response, next) {
 
 apiRoutes.post('/transactions', function(request, response, next) {
   // Pull transactions for the Item for the last 30 days to the front-end
-  // Sam: I added a admin section here so that the transactions for the Item
-  // are also added to the admin database
   var startDate = moment().format('YYYY-MM-DD').substr(0,8) + '01';
   var endDate = moment().format('YYYY-MM-DD');
 
@@ -390,12 +374,9 @@ function updateTransactions(timePeriod, accessToken, userId, callbackFunction) {
                     bucketSpending[bucketName] += transaction.amount;
                 }
             } else {
-                // console.log("adding to bucket total: " + bucketTotal[bucket] + " + " + transaction.amount);
                 bucketTotal[bucketName] += transaction.amount;
             }
         });
-        //Get Bucket Spending
-        // console.log(bucketSpending);
         console.log('updated bucket totals for this month:');
         console.log(bucketTotal);
 
@@ -426,8 +407,6 @@ function updateTransactions(timePeriod, accessToken, userId, callbackFunction) {
                     Total: bucketTotal[bucket]/timePeriod
                 });
         }
-        // console.log('updated bucket spending for this month:');
-        // console.log(bucketSpending);
 
         callbackFunction();
 
